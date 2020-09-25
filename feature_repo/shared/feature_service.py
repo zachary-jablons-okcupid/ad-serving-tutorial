91b715b1765276abf4d4c7c1ab5fe5a180043fae
brad <bradaallen@gmail.com>
2020-09-25 14:32:30 -0700

    updating sample feature service

--- feature_repo/shared/feature_service.py
+++ feature_repo/shared/feature_service.py
@@ -1,10 +1,8 @@
 from tecton import FeatureService, FeaturesConfig
 from feature_repo.shared.features.ad_ground_truth_ctr_performance_7_days import ad_ground_truth_ctr_performance_7_days
-from feature_repo.shared.features.user_partner_impression_count_7_days import user_partner_impression_count_7_days
-from feature_repo.shared.features.ad_group_ctr_performance import ad_group_ctr_performance
 from feature_repo.shared.features.user_total_ad_frequency_counts import user_total_ad_frequency_counts
 # from feature_repo.shared.features.ad_is_displayed_as_banner import ad_is_displayed_as_banner
-# from feature_repo.shared.features.user_ad_impression_counts import user_ad_impression_counts
+from feature_repo.shared.features.user_ad_impression_counts import user_ad_impression_counts
 
 
 ctr_prediction_service = FeatureService(
@@ -13,9 +11,8 @@ ctr_prediction_service = FeatureService(
     online_serving_enabled=True,
     features=[
         ad_ground_truth_ctr_performance_7_days,
-        user_partner_impression_count_7_days,
         user_total_ad_frequency_counts,
-        ad_group_ctr_performance,
+        user_ad_impression_counts
     ],
     family='ad_serving',
     tags={'release': 'production'},
